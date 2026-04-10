f93cd84623dfa00ce8ce24eb98296d1b256c07d6
Houssem25251 <daniel.delopi@gmail.com>
2026-04-10 19:30:44 +0100

    Update server.js

--- E-Book-Backend/server.js
+++ E-Book-Backend/server.js
@@ -7,8 +7,7 @@ import {usersRouter} from './routes/usersRoute.js';
 const app=express();
 app.use(express.json());
 app.use(cors({
-  "http://localhost:5173",
-  "https://e-book-vf1v.onrender.com"
+  origin:"https://e-book-vf1v.onrender.com"
 }));
 
 app.use('/images',express.static('booksdata/images'));
