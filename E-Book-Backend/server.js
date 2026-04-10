185c44432324af840946660d7b043b4bf1485d84
Houssem25251 <daniel.delopi@gmail.com>
2026-04-10 20:08:39 +0100

    Update server.js

--- E-Book-Backend/server.js
+++ E-Book-Backend/server.js
@@ -4,22 +4,18 @@ import cors from 'cors';
 import {booksRouter} from './routes/booksRoute.js';
 import {usersRouter} from './routes/usersRoute.js';
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:"https://e-book-vf1v.onrender.com"
+  origin: "https://e-book-vf1v.onrender.com"
 }));
 
-app.use('/images',express.static('booksdata/images'));
-app.use('/pdf',express.static('booksdata/pdf'));
+app.use('/images', express.static('booksdata/images'));
+app.use('/pdf', express.static('booksdata/pdf'));
 
+app.use('/api/books', booksRouter);
+app.use('/api/users', usersRouter);
 
-app.use('/api/books',booksRouter);
-app.use('/api/users',usersRouter);
-app.listen(process.env.PORT,()=>{console.log(`Server is up and running on Port:${process.env.PORT}`)});
-
-
-
-
-
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server is up and running on Port:${process.env.PORT}`);
+});
